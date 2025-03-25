ea84e7f2835ca30a8b7e668e1ad581fccb979251
Rithik Kumar <rithikarchadefacilator@gmail.com>
2025-03-25 04:20:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 
-//this usecase give you empty value
+// this use case give you empty value 
 // const height = parseInt(document.querySelector('#height').value);
 
 form.addEventListener('submit', function (e){
@@ -10,25 +10,28 @@ form.addEventListener('submit', function (e){
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
 
-    if(height === '' || height < 0 || isNaN(height)){
-        results.innerHTML = "Please give a valid height";
-    } else if(weight === '' || weight < 0 || isNaN(weight)){
-        results.innerHTML = "Please give a valid weight";
-    }else{
-        const bmi = (weight / ((height + height)/10000)).toFixed(2);
-        
-        //show the result
-        // results.innerHTML = `<span>${bmi}</span>` // displaying the result thorough this
+    // Checking 
+    if (height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = `Please give a valid Height ${height}`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid Weight ${weight}`;
+    } else{
+        const bmi = (weight / ((height*height) / 1000)).toFixed(2);
+
+        // Show the result 
+        results.innerHTML = `<span>${bmi}</span>`
 
         if(bmi <= 18.6){
-            results.innerHTML = `<span>Your BMI is ${bmi} and you are Under Weight</span>`;
+            results.innerHTML = `<span>Your BMI is ${bmi} and you are Under Weight</span>`
         }
-        if(bmi <= 18.6 && bmi >= 24.9){
-            results.innerHTML = `<span>Your BMI is ${bmi} and you are Under Weight</span>`;
+        else if(bmi <= 18.6 && bmi >= 24.9){
+            results.innerHTML = `<span>Your BMI is ${bmi} and you are in Normal Range</span>`
         }
-        if(bmi >= 24.9){
-            results.innerHTML = `<span>Your BMI is ${bmi} You are Over Weight</span>`
+        else if(bmi >= 24.9){
+            results.innerHTML = `<span>Your BMI is ${bmi} and you are Over Weight</span>`
+        }
+        else{
+            results.innerHTML = `<span>Your BMI is ${bmi} and not Listed in Website</span>`
         }
     }
 })
-
